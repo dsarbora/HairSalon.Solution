3176930fcb60eea5ae088e7ed27af9596301b295
Dave Sarbora <dsarbora@gmail.com>
2019-03-01 13:21:40 -0800

    add customer controller

--- HairSalon/Controllers/CustomersController.cs
+++ HairSalon/Controllers/CustomersController.cs
@@ -8,6 +8,45 @@ namespace HairSalon.Controllers
 {
     public class CustomersController : Controller
     {
-        
+        [HttpGet("/employee/{id}/customers/new")]
+        public ActionResult New(int id)
+        {
+            Employee searchedEmployee = Employee.Find(id);
+            return View(searchedEmployee);
+        }
+
+        [HttpGet("/employee/{employeeId}/customers/{id}")]
+        public ActionResult Show(int employeeId, int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Employee searchedEmployee = Employee.Find(employeeId);
+            Customer searchedCustomer = Customer.Find(id);
+            model.Add("employee", searchedEmployee);
+            model.Add("customer", searchedCustomer);
+            return View(model);
+        }
+
+        [HttpGet("/employee/{employeeId}/customers/{id}/edit")]
+        public ActionResult Edit(int id, int employeeId)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Employee searchedEmployee = Employee.Find(employeeId);
+            Customer searchedCustomer = Customer.Find(id);
+            model.Add("employee", searchedEmployee);
+            model.Add("customer", searchedCustomer);
+            return View(model);
+        }
+
+        [HttpPost("/employee/{employeeId}/customers/{id}")]
+        public ActionResult Update(int id, int employeeId, string name)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Customer searchedCustomer = Customer.Find(id);
+            searchedCustomer.Edit(name);
+            Employee searchedEmployee = Employee.Find(employeeId);
+            model.Add("employee", searchedEmployee);
+            model.Add("customer", searchedCustomer);
+            return View("Show", model);
+        }
     }
 }
