04e0729f002ae22d78af0ee23470d838fc09e2f3
Dave Sarbora <dsarbora@gmail.com>
2019-03-08 13:01:08 -0800

    add specialty tests and initial methods

--- HairSalon.Tests/ModelTests/Specialty.Test.cs
+++ HairSalon.Tests/ModelTests/Specialty.Test.cs
@@ -8,11 +8,25 @@ namespace HairSalon.Tests
     [TestClass]
     public class SpecialtyTest : IDisposable
     {
+
+        public SpecialtyTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root; password=root;port=8889;database=first_last_test;";
+        }
         public void Dispose()
         {
             Specialty.ClearAll();
             Customer.ClearAll();
             Employee.ClearAll();
+        }
+
+        [TestMethod]
+        public void Save_SavesSpecialtyToDatabase()
+        {
+            Specialty newSpecialty = new Specialty("Mullet");
+            newSpecialty.Save();
+            Specialty foundSpecialty = Specialty.GetAll()[0];
+            Assert.AreEqual(newSpecialty, foundSpecialty);
         }
     }
 }
